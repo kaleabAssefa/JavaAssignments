2b8c2f606ef3c59d8a75c8e70d0c2aec31589365
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-07-28 22:37:10 -0400

    Adding files

---
+++ src/com/sms/service/StudentServiceImpl.java
@@ -0,0 +1,16 @@
+package com.sms.service;
+
+import java.util.Scanner;
+
+public class StudentServiceImpl implements StudentService{
+    Scanner scanner = new Scanner(System.in);
+    List<Student>addStudents = new ArrayList<Student>();
+
+    @Override
+    public void addStudentInfo(){
+        System.out.println("Enter Student Number");
+        int studentNo=
+
+    }
+
+}
