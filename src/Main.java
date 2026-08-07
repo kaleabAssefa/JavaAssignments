4fea6435d3efcef68497b81761e1a018b25421d1
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-08-07 16:29:49 -0400

    Small shopping cart

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
-
+import java.util.Scanner;
 public class Main {
-
-
-    static int addingSum(int a, int b) {
-        return a + b;
-    }
-
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(addingSum(3, 4));
+
+        System.out.print("What item would you like to buy?: ");
+        String foodItem=sc.nextLine();
+        System.out.print("what is the price for each?: ");
+        double price = sc.nextDouble();
+        System.out.print("How many would like?: ");
+        int quantity=sc.nextInt();
+
+        double finalPrice= price*quantity;
+
+        System.out.println("\nYou have bought "+quantity+" "+ foodItem+"/s");
+        System.out.println("Your total is $"+finalPrice);
+        sc.close();
     }
+
 }
