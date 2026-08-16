e969883bfdb32dd3eb62d699a6051be71a9adcf5
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-08-15 21:38:35 -0400

    Adding a small game project

---
+++ src/RockPaperGame.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+    }
+
+}
+
