fb02f4d3f05f2f1c9d9f635c0f55821b4a0b9b3e
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-08-16 22:35:17 -0400

    Added a new feature where users are required to insert their atm number and atm pin

--- src/BankingSystem.java
+++ src/BankingSystem.java
@@ -2,67 +2,74 @@ import java.util.Scanner;
 
 public class BankingSystem {
     public static void main(String[] args) {
-         //The steps to create the banking system is
-        //step1: create the necssary variables
-        //Show the Menu
-        //step2: create methods after taking the users input
-        // The methods are 1,ShowBalance  2,Deposit  3,Withdraw  4,exit
         Scanner scanner = new Scanner(System.in);
-        double balance =100;
-        double amount;
-        int choice = 0;
-        while(choice !=4) {
-        System.out.println("*************************");
-        System.out.println("Welcome to Bankof America");
-        System.out.println("*************************");
-        System.out.println("1. Show Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Exit");
-        System.out.print("Enter your choice: ");
-        choice = scanner.nextInt();
-        double newBalance;
+        int atmPin = 12345;
+        int atm = 123;
 
-
-            switch (choice) {
-
-                case 1:
-                    System.out.println("Your current balance is: $" + balance);
-                    System.out.println(" ");
-                    break;
-                case 2:
-                    System.out.print("Enter how much you want to deposit: ");
-                    amount = scanner.nextDouble();
-                    System.out.println("You deposited $" + amount + "your now balance is $" + depositAmount(balance, amount));
-                    balance = depositAmount(balance, amount);
-                    break;
-                case 3:
-                    System.out.print("Enter how much you want to withdraw: ");
-                    amount = scanner.nextDouble();
-                    if (amount <= balance) {
-                        System.out.printf("You withdraw $%.2f  your new balance is $%.2f" ,amount, withDraw(balance, amount));
-                        balance = depositAmount(balance, amount);
-                    } else {
-                        System.out.println("You cannot withdraw more than your current balance");
+        double balance = 100;
+        int options = 1;
+        double amount = 0;
+        System.out.print("Please Enter your Atm number: ");
+        int userAtmPin = scanner.nextInt();
+        if (atmPin == userAtmPin) {
+            System.out.print("Please enter you atm pin: ");
+            int userPin = scanner.nextInt();
+            if (atm == userPin) {
+                while (options != 4) {
+                    System.out.println("******************");
+                    System.out.println("BANKING PROGRAM");
+                    System.out.println("******************");
+                    System.out.println("1. Show Balance");
+                    System.out.println("2. Deposit");
+                    System.out.println("3. Withdraw");
+                    System.out.println("4. Exit");
+                    System.out.println("******************");
+                    System.out.print("Enter your choice: ");
+                    options = scanner.nextInt();
+                    switch (options) {
+                        case 1:
+                            System.out.println("Your balance is $" + balance);
+                            break;
+                        case 2:
+                            System.out.print("Enter how much you want to deposit: ");
+                            amount = scanner.nextDouble();
+                            balance = deposit(amount, balance);
+                            System.out.println("Your balance is: $" + balance);
+                            break;
+                        case 3:
+                            System.out.print("How much money you want to with draw?: ");
+                            amount = scanner.nextDouble();
+                            if (amount <= balance) {
+                                balance = withdraw(amount, balance);
+                                System.out.println("Your current balance is: " + balance);
+                            } else {
+                                System.out.println("You cant withdraw more than your balance");
+                            }
+                            break;
+                        case 4:
+                            System.out.println("Thanks for choosing our Bank");
+                            break;
+                        default:
+                            System.out.println("Please choose the right menu");
                     }
-                case 4:
-                    System.out.println(" ");
-                    System.out.println("GoodBye!");
-                default:
-                    System.out.println("Thank you for choosing BOFA");
-                    System.out.println(" ");
+                }
 
+            } else {
+                System.out.println("Wrong pin");
             }
+        } else {
+            System.out.println("Wrong Atm number try again!");
         }
+        scanner.close();
     }
 
-    static double depositAmount(double balance, double amount){
-         return balance += amount;
+    static double deposit(double amount, double balance) {
+        return balance += amount;
 
     }
-    static double withDraw(double balance,double amount){
-        double finalAmount = balance - amount;
-        return finalAmount;
+
+    static double withdraw(double amount, double balance) {
+        return balance -= amount;
     }
 
 }
